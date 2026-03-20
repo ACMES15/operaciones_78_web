8fc47a1fd2f55d55c8936ea11a27d841fb82cb39
ACMES15 <acmess24@gmail.com>
2026-03-19 22:08:23 -0600

    mensajes 1.3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "905817e99cab06688095d259c94f2677
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "cd5a9bb52814b8fd58603d38ba9a1061",
+"flutter_bootstrap.js": "5de94e90ac5cfba0e3e8b49451ea57bb",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
