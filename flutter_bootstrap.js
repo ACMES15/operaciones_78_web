24654b608b59084b24c8be5234ddefbb87b8fe6f
ACMES15 <acmess24@gmail.com>
2026-04-12 15:07:18 -0600

    actualizaciones 3.9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3094976035"
+    serviceWorkerVersion: "220604557"
   }
 });
